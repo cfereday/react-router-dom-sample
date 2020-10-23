b8cb4e588b8b507e0c8950f92e5912feab90921d
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-10-23 20:11:57 +0200

    Add react router & scaffolding links
    We added in basic links that we know lead to nowhere!
    
    The basic router scaffold is there
    
    Signed-off-by: Charlotte Fereday <charlotte.fereday@tesglobal.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {  BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>PLEASE LINK :D </h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
